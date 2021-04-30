3a09003d9e7c8150b60365e225b17d776cf346c9
Pedro Resende <pedroresende@cpejr.com.br>
2021-04-30 15:30:59 -0300

    alterei o login para lincar as paginas

--- src/paginas/Home/Home.js
+++ src/paginas/Home/Home.js
@@ -4,14 +4,14 @@ import {Link} from "react-router-dom";
 function Home() {
     return( 
         <div className = "Home">
-    <h1 className = "Home"> Home </h1>
-    <Link to="cadastro">Cadastro</Link><h1> </h1>
-    <Link to="empreendimentos">Empreendimentos</Link><h1> </h1>
-    <Link to="empresas">Empresas</Link><h1> </h1>
-    <Link to="login">Login</Link><h1> </h1>
-    <Link to="perfil">Perfil</Link><h1> </h1>
-    <Link to="quemsomos">Quem Somos</Link><h1> </h1>
-    </div>
+        <h1 className = "Home"> Home </h1>
+        <Link to="cadastro">Cadastro</Link><h1> </h1>
+        <Link to="empreendimentos">Empreendimentos</Link><h1> </h1>
+        <Link to="empresas">Empresas</Link><h1> </h1>
+        <Link to="login">Login</Link><h1> </h1>
+        <Link to="perfil">Perfil</Link><h1> </h1>
+        <Link to="quemsomos">Quem Somos</Link><h1> </h1>
+        </div>
 
     );
 }
--- src/paginas/Login/Login.js
+++ src/paginas/Login/Login.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import {Form, Button} from "react-bootstrap"
 import "./Login.css"
 import {Link} from "react-router-dom";
+import {useHistory} from "react-router-dom"
 
 
 function Login() {
+    const[email, setEmail] = useState();
+    const[password, setPassword] = useState();
+    const history = useHistory();
+        function login(){
+            alert("aopa"+ email+ " " + password);
+            history.push("perfil");
+        }
     return ( 
-<div className = "box">
+    <div className = "box">
     <div className ="comteudo">
     <Form>
     <h1> RESENDE BUSINESS </h1>
     <Form.Group controlId="formBasicEmail">
-    <Form.Control type="email" placeholder="Email" />
+    <Form.Control type="email" placeholder="Email" onChange={(e)=>setEmail(e.target.value)} />
   </Form.Group>
-    <Form.Control type="password" placeholder="Senha" />
+    <Form.Control type="password" placeholder="Senha" onChange={(e)=>setPassword(e.target.value)} />
     <p></p>
-    <Button variant="primary">Login</Button>
+    <Button variant="primary" onClick={login}>Login</Button>
     <p></p>
     <Link to="cadastro">Nao é cadastrado?</Link><h1> </h1>
     <Link to=" ">Esqueci minha senha.</Link><h1> </h1>
