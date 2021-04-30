dc442b49beba9d95ee5354f7dad2adaa54ad96cb
Ezequiel Moreira <ezequielmoreira@cpejr.com>
2021-04-29 22:04:57 -0300

    Quemsomos

--- src/paginas/QuemSomos/QuemSomos.js
+++ src/paginas/QuemSomos/QuemSomos.js
@@ -3,31 +3,24 @@ import "./QuemSomos.css";
 
 
 function QuemSomos() {
-  return (
-   
-    <div className="Pagina">      
-      <h1 className="Titulo">Quem somos</h1>
-      <h2 className = "Subtitulo"> Empresa: </h2>
-     <img src="/imagem/foto.jpg" alt=""/>
+  return ( 
+  <body>
 
-<div className = "Meio"> 
-     
-<img src="/imagem/foto.jpg" alt=""/>
-<h1> Pessoas são assim </h1>
+  
+    
 
-</div>
 
-<div> 
-<img src="/imagem/foto.jpg" alt=""/>
-    <h1> Dúvidas Frequentes</h1>
+<div className="box"> 
 
+<h1> Olá mundo </h1>
 
 
 </div>
 
 
-    </div>
 
+
+        </body>
   )
 
   }
