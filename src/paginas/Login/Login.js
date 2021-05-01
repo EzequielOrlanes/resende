5f10fa67b0a161a77332d764a60aab5d40f5f602
Ezequiel Moreira <ezequielmoreira@cpejr.com>
2021-05-01 15:15:16 -0300

    Estilização do login e CSS global funcionando perfeitamente

--- src/paginas/Login/Login.js
+++ src/paginas/Login/Login.js
@@ -1,27 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 import {Form, Button} from "react-bootstrap"
 import "./Login.css"
 import {Link} from "react-router-dom";
+import {useHistory} from "react-router-dom"
 
 
 function Login() {
-    return ( 
-<div className = "box">
-    <div className ="comteudo">
-    <Form>
-    <h1> RESENDE BUSINESS </h1>
-    <Form.Group controlId="formBasicEmail">
-    <Form.Control type="email" placeholder="Email" />
-  </Form.Group>
-    <Form.Control type="password" placeholder="Senha" />
-    <p></p>
-    <Button variant="primary">Login</Button>
-    <p></p>
-    <Link to="cadastro">Nao é cadastrado?</Link><h1> </h1>
-    <Link to=" ">Esqueci minha senha.</Link><h1> </h1>
-    </Form>
+
+    const[email, setEmail] = useState();
+    const[password, setPassword] = useState();
+    const history = useHistory();
+    function login(){
+    alert("aopa"+ email+ " " + password);
+    history.push("perfil");
+}
+
+return ( 
+<div className = "Pagina-login"> 
+
+   <div className= "box-logomarca"> </div>
+
+    <div className = "box-login">
+                <Form>
+                <h1 className = "Titulo-login"> Resende Business</h1>
+                <p></p>
+                <div className="Conteudo-box-login">
+                <p className = "Subtitulo-login"> Faça seu Login: </p> 
+
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Control type="email" placeholder="Email" onChange={(e)=>setEmail(e.target.value)} />
+                </Form.Group>
+
+                <Form.Control type="password" placeholder="Senha" onChange={(e)=>setPassword(e.target.value)} />
+                    <p></p>
+                    
+                    <Button  variant="primary" onClick={login}>ENTRAR</Button>
+                    
+                    <p></p>
+
+                    <Link to="cadastro"> Não é cadastrado? </Link><h1> </h1>
+                    <Link to=" ">Esqueci minha senha</Link><h1> </h1>
+                    </div>
+                </Form>
+
     </div>
 </div>
+
+
     )
 }
 
--- src/paginas/QuemSomos/QuemSomos.js
+++ src/paginas/QuemSomos/QuemSomos.js
@@ -4,29 +4,22 @@ import "./QuemSomos.css";
 
 function QuemSomos() {
   return ( 
-  <body>
+<div className="Pagina-QuemSomos"> 
 
-  
-    
-
-
-<div className="box1"> 
-
+    <div className = "box-quemsomos">
 <h1 className = "Quemsomosapresentação"> Quem somos nós ? </h1>
-
 <p className = "Textodeapresentação"> Nós somos alunos de graduação de Engenharias Elétrica e Controle e Automação da Universidade Federal de Minas Gerais(UFMG), e 
   atualmente somos trainees da CPE - Consultoria e Projetos Elétricos, uma empresa júnior sem fins lucrativos que busca incentivar o empreendedorismo no Brasil. Nosso objetivo com o ... é dar visibilidade aos microeemprendedores da cidade de Resende Costa (MG) que começaram a surgir principalmente após a pandemia da Covid-19, a fim de impulsionar o comércio da cidade local.</p>
 
 <img id = "foto" src= "imagem/foto.jpg" alt=""/>
 
 
-</div>
 
+    </div>
 
+</div>    
 
-
-        </body>
-  )
+)
 
   }
 
