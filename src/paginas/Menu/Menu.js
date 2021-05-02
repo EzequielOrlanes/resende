683663e508c0d76e3777b98cef912bd91fd68f07
Júlia de Castro <juliadecastro777@gmail.com>
2021-05-02 19:55:28 -0300

    Inseri a Barra de Menu de Navegação

---
+++ src/paginas/Menu/Menu.js
@@ -0,0 +1,87 @@
+import React, {useState} from "react";
+import {Drawer, List, ListItem, ListItemText, Typography, IconButton, AppBar, Toolbar, Avatar} from  "@material-ui/core";
+import {MdHome, MdPerson, MdLaptop, MdMenu} from "react-icons/md";
+import { IconContext } from "react-icons/lib";
+import "./Menu.css";
+import {useHistory} from "react-router-dom";
+
+function Menu (props){
+    const history = useHistory();
+    const [currentPage, setCurrentPage] = useState("/home");
+    const [open, setOpen] = useState(false);
+    function handleClick(pathName){
+        history.push(pathName);
+        setCurrentPage(pathName)
+
+    }
+
+    function handleDrawer(isOpen){
+        setOpen(isOpen)
+    }
+
+    const pages = [
+    {
+        pathName: "/home",
+        icon: <MdHome/>,
+        text: "Home",
+
+    },
+
+    {
+        pathName: "/perfil",
+        icon: <MdPerson/>,
+        text: "Perfil",
+
+    },
+
+    {
+        pathName: "/login",
+        icon: <MdLaptop/>,
+        text: "Login",
+
+    },
+];
+
+    return (
+        <>
+        <AppBar position="statc">
+            {props.children}
+            <Toolbar style= {{backgroundColor: "white"}}>
+            <IconButton
+            edge="start" 
+            aria-label="menu"
+            onClick ={() => handleDrawer(!open)}>
+        <MdMenu/>
+        </IconButton>
+            <div className="userContainer">
+            <p className="userName">Resende Business</p>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </div>
+            </Toolbar>
+        </AppBar>
+        <Drawer open={open} onClose={() => handleDrawer(false)}>
+           <List className="Drawer">
+               {pages.map((listItem)=> {
+                   return(
+               <ListItem 
+               button
+               selected = {currentPage === listItem.pathName}
+                onClick={()=> {handleClick(listItem.pathName)}}>
+                    <IconContext.Provider value={{color: "#343434", size: "1.5em"}}>  
+                        {listItem.icon}
+                    </IconContext.Provider>
+                        <ListItemText className = "listText">
+                            <Typography>{listItem.text}</Typography>
+                        </ListItemText>
+               </ListItem>
+               
+               );
+               })}
+               
+           </List>
+        </Drawer>
+        </>
+    );
+}
+
+export default Menu;
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Cadastro from "./paginas/Cadastro";
 import Empreendimentos from "./paginas/Empreendimentos";
 import Empresas from "./paginas/Empresas";
@@ -7,23 +7,34 @@ import Home from "./paginas/Home";
 import Login from "./paginas/Login";
 import Perfil from "./paginas/Perfil";
 import QuemSomos from "./paginas/QuemSomos";
-
+import Menu from "./paginas/Menu";
 
 function Routes(){
     return(
         <BrowserRouter>
             <Switch>           
                 <Route path="/Login" component={Login}/>
-                <Route path="/Home" component={Home}/>
                 <Route path="/Empreendimentos" component={Empreendimentos}/>
                 <Route path="/Empresas" component={Empresas}/>
                 <Route path="/Perfil" component={Perfil}/>
                 <Route path="/Cadastro" component={Cadastro}/>
                 <Route path="/QuemSomos" component={QuemSomos}/>
+                <Route path="/" component={UserMenu}/>
             </Switch>
         </BrowserRouter>
 
     );
+}
+
+function UserMenu(){
+    return (
+        <Menu>
+           <Switch>           
+                <Route path="/home" component={Home}/>
+                <Route component={() => <Redirect to="/home"></Redirect>}/>
+            </Switch> 
+        </Menu>
+    )
 }
 
 export default Routes;
