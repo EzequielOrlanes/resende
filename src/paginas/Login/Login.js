78c9c1bacb1d06d254dbe9da90b6b6958a943e7e
Matheus <matheusmiranda98@live.com>
2021-05-03 09:58:55 -0300

    Formatação

--- src/paginas/Login/Login.js
+++ src/paginas/Login/Login.js
@@ -6,7 +6,6 @@ import {useHistory} from "react-router-dom"
 
 
 function Login() {
-
     const[email, setEmail] = useState();
     const[password, setPassword] = useState();
     const history = useHistory();
@@ -15,38 +14,28 @@ function Login() {
     history.push("perfil");
 }
 
-return ( 
-<div className = "Pagina-login"> 
-
-   <div className= "box-logomarca"> </div>
-
-    <div className = "box-login">
+    return ( 
+        <div className = "Pagina-login"> 
+        <div className= "box-logomarca"></div>
+            <div className = "box-login">
                 <Form>
-                <h1 className = "Titulo-login"> Resende Business</h1>
-                <p></p>
-                <div className="Conteudo-box-login">
-                <p className = "Subtitulo-login"> Faça seu Login: </p> 
-
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Email" onChange={(e)=>setEmail(e.target.value)} />
-                </Form.Group>
-
-                <Form.Control type="password" placeholder="Senha" onChange={(e)=>setPassword(e.target.value)} />
+                    <h1 className = "Titulo-login"> Resende Business</h1>
                     <p></p>
-                    
-                    <Button  variant="primary" onClick={login}>ENTRAR</Button>
-                    
-                    <p></p>
-
-                    <Link to="cadastro"> Não é cadastrado? </Link><h1> </h1>
-                    <Link to=" ">Esqueci minha senha</Link><h1> </h1>
+                    <div className="Conteudo-box-login">
+                        <p className = "Subtitulo-login"> Faça seu Login: </p> 
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Control type="email" placeholder="Email" onChange={(e)=>setEmail(e.target.value)} />
+                        </Form.Group>
+                            <Form.Control type="password" placeholder="Senha" onChange={(e)=>setPassword(e.target.value)} />
+                        <p></p>
+                        <Button  variant="primary" onClick={login}>ENTRAR</Button>
+                        <p></p>
+                        <Link to="cadastro"> Não é cadastrado? </Link><h1> </h1>
+                        <Link to=" ">Esqueci minha senha</Link><h1> </h1>
                     </div>
                 </Form>
-
-    </div>
-</div>
-
-
+            </div>
+        </div>
     )
 }
 
