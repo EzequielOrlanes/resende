85350044abeec1943b6b194c6feb7c40c6d81bbe
Pedro Resende <pedroresende@cpejr.com.br>
2021-05-04 15:04:37 -0300

    aprimorar cadastro

--- src/paginas/Cadastro/Cadastro.js
+++ src/paginas/Cadastro/Cadastro.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Form} from "react-bootstrap"
+import {Form, Col,Button} from "react-bootstrap"
 import "./Cadastro.css"
 
 function Cadastro() {
@@ -28,11 +28,46 @@ function Cadastro() {
                                 <Form.Label>CNPJ</Form.Label>
                                 <Form.Control type="name" placeholder="EX: 99.999.999/9999-99" />
                             </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Qual a área de atuação do seu empreendimento</Form.Label>
                                 <Form.Control as="select" size="lg">
-                                <option>tipo 1</option>
-                                <option>tipo 2</option>
-                                <option>tipo 3</option>
+                                <option>Escolha...</option>  
+                                <option>Artesanato</option>
+                                <option>Alimentos</option>
+                                <option>Vestuário</option>
+                                <option>Produtos para casa</option>
                             </Form.Control>
+                            </Form.Group>
+                            <Form.Group controlId="formBasicName">
+                                <Form.Label>Telefone para contato</Form.Label>
+                                <Form.Control type="name" placeholder="EX: (99)99999-9999" />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicName">
+                            <Form.Label>Endereço</Form.Label>
+                                <Form.Control type="name" placeholder="Av. Primeiro" />
+                            </Form.Group>
+                            <Form.Row>
+                                <Form.Group as={Col} controlId="formGridName">
+                                    <Form.Label>Numero</Form.Label>
+                                    <Form.Control type="name" placeholder="Numero" />
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridName">
+                                    <Form.Label>Complemento</Form.Label>
+                                    <Form.Control type="name" placeholder="Complemento" />
+                                </Form.Group>
+                            </Form.Row>
+
+                            <Form.Group controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Nos fale sobre seu empreendimento</Form.Label>
+                                <Form.Control as="textarea" rows={3} />
+                            </Form.Group>
+                            <Form.Group id="formGridCheckbox">
+                                <Form.Check type="checkbox" label="I'm not a robot" />
+                            </Form.Group>
+                            <Button variant="primary" size="lg" block>
+                                Cadastrar 
+                            </Button>
                         </Form>
                     </div>
                 </div>
