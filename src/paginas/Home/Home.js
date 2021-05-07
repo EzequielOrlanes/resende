8847f3fbc9184329766166200c60037ac60b3017
Júlia de Castro <juliadecastro777@gmail.com>
2021-05-06 22:35:49 -0300

    Atualizando a Home

--- src/paginas/Home/Home.js
+++ src/paginas/Home/Home.js
@@ -18,8 +18,27 @@ function Home() {
             <div className = "Home">
                 
             </div>
-                <div className ="content"> Bloco 2
-                    </div> 
+
+            <div className = "bloco2"><h1>Empreendimentos Destaques do Mês</h1> </div> 
+                <div className ="content">
+                     
+                    <>
+                    <div className = "titulos">
+                        <h1 className = "nome">#1 Segredos de Minas</h1>
+                        <h1 className = "nome">#2 Trem de Minas Decor</h1>
+                        <h1 className = "nome">#3 Tricô da Vovó</h1>
+                    </div>
+
+                    <div className = "images">
+                    
+                        <img className="box1" src = "/imagem/1.jpg"/>
+                        <img className="box1" src = "/imagem/2.jpg"/>
+                        <img className="box1" src = "/imagem/3.jpg"/>
+                        </div>
+                        </>
+                    
+                    </div>
+                    
                         <div className ="footer"> 
                             <p> 
                             Resende Business - Avenida Barbacena.
