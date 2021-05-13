000c996932d875f2ea21f9eb015265e0f9086750
Julia <julianagem@cpejr.com.br>
2021-05-13 11:12:12 -0300

    duvidas frequentes

--- src/paginas/QuemSomos/QuemSomos.js
+++ src/paginas/QuemSomos/QuemSomos.js
@@ -1,8 +1,26 @@
 import Menu from "../Menu"
 import React from "react";
 import "./QuemSomos.css";
+import { makeStyles } from '@material-ui/core/styles';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}));
+
 
 function QuemSomos() {
+  const classes = useStyles();
   return (           
 
   <div className="Pagina-QuemSomos"> 
@@ -35,6 +53,37 @@ function QuemSomos() {
         <div className = "Caixa2">
           <div className= "header-duvidas"> 
             <h1 className = "Titulo-duvidas">Dúvidas Frequentes</h1> </div>
+              <div className = "acordeao">
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <div className="duvida1">Não sou de Resende Costa. Posso me inscrever no site?</div>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <div className="resposta1">
+                      Infelizmente não. A plataforma é destinada apenas a empreendedores que residem em Resende Costa.
+                    </div>
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel2a-content"
+                    id="panel2a-header"
+                  >
+                    <div className="duvida2">Quais são os requesitos para me inscrever?</div>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <div className="resposta2">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                      sit amet blandit leo lobortis eget.
+                   </div>
+                  </AccordionDetails>
+                </Accordion>
+              </div>
           </div>
         </div>
 
