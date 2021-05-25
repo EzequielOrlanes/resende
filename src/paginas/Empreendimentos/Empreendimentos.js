9243871c02894814c90a9124754a6c1d613fc7dd
Julia <julianagem@cpejr.com.br>
2021-05-25 15:15:56 -0300

    Mais empreendimentos

--- src/paginas/Empreendimentos/Empreendimentos.js
+++ src/paginas/Empreendimentos/Empreendimentos.js
@@ -39,14 +39,14 @@ function Empreendimentos() {
               <CardActionArea>
                 <CardMedia
                   className={classes.media}
-                  image="/imagem/almofada.png"
+                  image="/imagem/1.jpg"
                 />
                 <CardContent>
                   <div className="titulo-empreen">Segredos de Minas</div>
                   <div className="descricao-empreen">
                     <p></p>
                     Quer deixar sua casa ainda mais bonita? Nos dedicamos a criar
-                     itens de decoração criativos e que valorizarão ainda mais o ambiente.
+                     itens de decoração criativos e que valorizam ainda mais o ambiente.
 
                   </div>
                 </CardContent>
@@ -63,7 +63,7 @@ function Empreendimentos() {
               <CardActionArea>
                 <CardMedia
                   className={classes.media}
-                  image="/imagem/almofada.png"
+                  image="/imagem/img8.jpg"
                 />
                 <CardContent>
                   <div className="titulo-empreen">Tricô da Vovó</div>
@@ -86,10 +86,152 @@ function Empreendimentos() {
               <CardActionArea>
                 <CardMedia
                   className={classes.media}
-                  image="/imagem/almofada.png"
+                  image="/imagem/2.jpg"
                 />
                 <CardContent>
                   <div className="titulo-empreen">Trem de Minas Decor</div>
+                  <div className="descricao-empreen">
+                    <p></p>
+                    Decoração para que cada cantinho da sua casa fique ainda 
+                    mais estiloso! Aceitamos encomendas de produtos customizados.
+                  </div>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="medium" color="primary">
+                  <Link to="tremdeminasdecor">Saiba mais</Link>
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
+        </div>
+        <div className="centralizar">
+          <div className="card1">
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="/imagem/img6.jpg"
+                />
+                <CardContent>
+                  <div className="titulo-empreen">Quadros da Maria</div>
+                  <div className="descricao-empreen">
+                    <p></p>
+                    Mande sua foto para a gente e criaremos quadros 
+                    personalizados para enfeitar sua casa! Também temos modelos prontos.
+                  </div>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="medium" color="primary">
+                  Saiba mais
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
+          <div className="card2">
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="/imagem/img5.jpg"
+                />
+                <CardContent>
+                  <div className="titulo-empreen">Louças Resende</div>
+                  <div className="descricao-empreen">
+                    <p></p>
+                    Vendemos louças manufaturadas com designs elegantes e singulares 
+                    para você servir seus convidados e  enfeitar sua mesa!
+                  </div>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="medium" color="primary">
+                  Saiba mais
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
+          <div className="card3">
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="/imagem/img4.jpg"
+                />
+                <CardContent>
+                  <div className="titulo-empreen">Bela Cozinha</div>
+                  <div className="descricao-empreen">
+                    <p></p>
+                    Panos de prato, porta talheres, descansos de panela 
+                    e diversos outros utensilhos de cozinha e decoração para o seu ambiente.
+                  </div>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="medium" color="primary">
+                  Saiba mais
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
+        </div>
+        <div className="centralizar">
+          <div className="card1">
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="/imagem/img3.jpg"
+                />
+                <CardContent>
+                  <div className="titulo-empreen">Sua Cerâmica</div>
+                  <div className="descricao-empreen">
+                    <p></p>
+                    Lojinha de produtos especializada na produção manual de vasos e 
+                    louças de cerâmica. Nada mais tradicional em Resende Costa!
+                  </div>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="medium" color="primary">
+                  Saiba mais
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
+          <div className="card2">
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="/imagem/img2.jpg"
+                />
+                <CardContent>
+                  <div className="titulo-empreen">Rainha do Tricô</div>
+                  <div className="descricao-empreen">
+                    <p></p>
+                    Venda de roupa personalizadas e feitas pelas mais experientes 
+                    costureiras da região. Venha conferir nossas lindas peças!
+                  </div>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="medium" color="primary">
+                  Saiba mais
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
+          <div className="card3">
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="/imagem/img1.jpg"
+                />
+                <CardContent>
+                  <div className="titulo-empreen">Decorar com Cores</div>
                   <div className="descricao-empreen">
                     <p></p>
                     Desde 2015 produzindo almofadas, tapetes, colchas, toalhas e 
@@ -99,7 +241,78 @@ function Empreendimentos() {
               </CardActionArea>
               <CardActions>
                 <Button size="medium" color="primary">
-                  <Link to="tremdeminasdecor">Saiba mais</Link>
+                  Saiba mais
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
+        </div>
+        <div className="centralizar">
+          <div className="card1">
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="/imagem/img7.jpg"
+                />
+                <CardContent>
+                  <div className="titulo-empreen">Garrafas color</div>
+                  <div className="descricao-empreen">
+                    <p></p>
+                    Arte feita com garrafas e areia de todas as cores para enfeitas 
+                    prateleiras, mesas e superficies e alegrar ainda mais o ambiente!
+                  </div>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="medium" color="primary">
+                  Saiba mais
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
+          <div className="card2">
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="/imagem/img9.jpg"
+                />
+                <CardContent>
+                  <div className="titulo-empreen">Produtos do Rafa</div>
+                  <div className="descricao-empreen">
+                    <p></p>
+                    Cada item feito com cuidado e criatividade para presentear seus 
+                    amigos e parentes! Já estamos te esperando na nossa loja!
+                  </div>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="medium" color="primary">
+                  Saiba mais
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
+          <div className="card3">
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="/imagem/arte.jpg"
+                />
+                <CardContent>
+                  <div className="titulo-empreen">Decorar com Cores</div>
+                  <div className="descricao-empreen">
+                    <p></p>
+                    Venha ver como são feitos nossas cerâmicas! Além de produtos 
+                    lindos para se comprar, ensinamos a fazê-los você mesmo!
+                  </div>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="medium" color="primary">
+                  Saiba mais
                 </Button>
               </CardActions>
             </Card>
